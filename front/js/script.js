ad161955ad914dbefa71fa580425e9037bdc5854
Medico Alexandre <alexandre.medico@hotmail.fr>
2020-06-16 14:48:50 +0200

    fini etoile, ajout new game et debut jeu

--- front/js/script.js
+++ front/js/script.js
@@ -1,47 +1,41 @@
 window.addEventListener("DOMContentLoaded", function() {
+    // etoile1
     setInterval(() => {
-        document.getElementById('etoile-1').classList.add('slide-in-top');
+        document.getElementById('etoile1').classList.add('slide-in-top-etoile1');
         setTimeout(() => {
-            document.getElementById('etoile-1').classList.remove('slide-in-top');
+            document.getElementById('etoile1').classList.remove('slide-in-top-etoile1');
         }, 1500);
     }, 3000);
-    // setInterval(() => {
-        // let i = 0;
-        // let j = 0;
-        // let etoile = setInterval(() => {
-        //     i++;
-        //     document.getElementById('etoile-1').insertAdjacentHTML('beforeend', `<div id='etoile${i}'></div>`);
-        //     document.getElementById(`etoile${i}`).classList.add('autreEtoile');
-        //     if (i == 500) {
-        //         clearInterval(etoile);
-        //         let supEtoile = setInterval(() => {
-        //             j++;
-        //             document.getElementById(`etoile${j}`).classList.remove('autreEtoile');
-        //             if (j == 500) {
-        //                 clearInterval(supEtoile);
-        //             }
-        //         }, 1);
-        //     }
-        // }, 1);
-    // }, 10000);
 
-    // setInterval(() => {
-    //     let i = 0;
-    //     let j = 0;
-    //     let etoile = setInterval(() => {
-    //         i++;
-    //         document.getElementById('menu').insertAdjacentHTML('beforeend', `<div id='etoile${i}'></div>`);
-    //         document.getElementById(`etoile${i}`).classList.add('autreEtoile');
-    //         if (i == 500) {
-    //             clearInterval(etoile);
-    //             let supEtoile = setInterval(() => {
-    //                 j++;
-    //                 document.getElementById(`etoile${j}`).classList.remove('autreEtoile');
-    //                 if (j == 500) {
-    //                     clearInterval(supEtoile);
-    //                 }
-    //             }, 1);
-    //         }
-    //     }, 1);
-    // }, 10000);
+    // etoile2
+    setInterval(() => {
+        document.getElementById('etoile2').classList.add('slide-in-top-etoile2');
+        setTimeout(() => {
+            document.getElementById('etoile2').classList.remove('slide-in-top-etoile2');
+        }, 1500);
+    }, 4000);
+
+    // etoile3
+    setInterval(() => {
+        document.getElementById('etoile3').classList.add('slide-in-top-etoile3');
+        setTimeout(() => {
+            document.getElementById('etoile3').classList.remove('slide-in-top-etoile3');
+        }, 1500);
+    }, 2000);
+
+    // etoile4
+    setInterval(() => {
+        document.getElementById('etoile4').classList.add('slide-in-top-etoile4');
+        setTimeout(() => {
+            document.getElementById('etoile4').classList.remove('slide-in-top-etoile4');
+        }, 1500);
+    }, 2500);
+
+    // etoile5
+    setInterval(() => {
+        document.getElementById('etoile5').classList.add('slide-in-top-etoile5');
+        setTimeout(() => {
+            document.getElementById('etoile5').classList.remove('slide-in-top-etoile5');
+        }, 1500);
+    }, 3500);
 })
